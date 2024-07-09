e557f399692bc88968b6f9a609c9d908394abbb2
putrapratama <putramiharjo236@gmail.com>
2024-07-09 09:14:40 +0700

    [PP] first commit

---
+++ pages/home.tsx
@@ -0,0 +1,14 @@
+// pages/home.tsx
+import React from 'react';
+import Navbar from '../components/navbar';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
